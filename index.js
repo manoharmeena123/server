275737b86abfec98670433e83cf56151b369783b
manohar meena <manoharmeena245@gmail.com>
2024-04-19 03:26:40 +0530

    task completed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { router } = require("./routes/router")
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json())
-app.use("/api", router);
+app.use("/", router);
 
 app.get("/", (req, res) => {
     res.send("Welcome..");
